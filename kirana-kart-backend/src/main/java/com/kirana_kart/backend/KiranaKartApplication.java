b75dc0df564da67ec89e683cce31edd674128f40
Atul Lahade <atul.lahade@mendix.com>
2026-01-02 00:08:36 +0530

    BackEnd project setup initial commit

---
+++ kirana-kart-backend/src/main/java/com/kirana_kart/backend/KiranaKartApplication.java
@@ -0,0 +1,13 @@
+package com.kirana_kart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaKartApplication.class, args);
+	}
+
+}
